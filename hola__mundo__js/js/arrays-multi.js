d8dfd890f5f70ef4103d99e2c1ae644462da4feb
GuzmanAaron <aaronguzman.dev@gmail.com>
2022-06-29 13:22:51 -0500

    arrays

---
+++ hola__mundo__js/js/arrays-multi.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var categorias = ["Acción", "Terror", "Comedia"];
+var peliculas = [" La verdad duele", "Blanca Nieves", "Pokemon"];
+
+var cine = [categorias, peliculas];
+
+console.log (cine[0][1]);
--- hola__mundo__js/js/arrays.js
+++ hola__mundo__js/js/arrays.js
@@ -7,6 +7,23 @@
 var nombre = "Aaron";
 var nombres = ["Victor", "Juan", "Peter", 23, true];
 
-var lenguajes = new Array("PHP", "JS", "JAVA");
+var lenguajes = new Array("PHP", "JS", "JAVA", "PHYTON");
 
-console.log(nombres[2]);
+// 
+document.write("<h1>Lenguaje de progración 2022</h1>");
+// document.write(lenguajes[0])
+
+document.write("<ul>");
+
+
+//  for( var i = 0; i < lenguajes.length; i++){
+//     document.write("<li>"+lenguajes[i]+"</li>")
+//  }
+
+
+lenguajes.forEach((elemento, indix, array)=>
+    document.write("<li>"+indix+" - "+elemento+"</li>")
+);
+
+
+ document.write("</ul>");
